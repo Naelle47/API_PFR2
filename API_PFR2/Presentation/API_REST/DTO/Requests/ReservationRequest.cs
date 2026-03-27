4339d0b48876a2936d332ea22f4e8cf22d6684c4
Naelle47 <benard.audrey1@gmail.com>
2026-03-27 08:46:54 +0100

    -

--- API_PFR2/Presentation/API_REST/DTO/Requests/ReservationRequest.cs
+++ API_PFR2/Presentation/API_REST/DTO/Requests/ReservationRequest.cs
@@ -9,20 +9,20 @@ public class CreateReservationRequest
     /// <summary>
     /// Gets or sets the identifier of the user making the reservation.
     /// </summary>
-    public int? UtilisateurId { get; set; }
+    public int UtilisateurId { get; set; }
 
     /// <summary>
     /// Gets or sets the identifier of the reserved game.
     /// </summary>
-    public int? JeuId { get; set; }
+    public int JeuId { get; set; }
 
     /// <summary>
     /// Gets or sets the reservation start date.
     /// </summary>
-    public DateTime? DateDebut { get; set; }
+    public DateTime DateDebut { get; set; }
 
     /// <summary>
     /// Gets or sets the reservation end date.
     /// </summary>
-    public DateTime? DateFin { get; set; }
+    public DateTime DateFin { get; set; }
 }
