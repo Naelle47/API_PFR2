6abf9e19d385b469de584a675a26e27e717472b9
Naelle47 <benard.audrey1@gmail.com>
2026-03-31 14:59:41 +0200

    - fix : add dataAnnotations on a few DTOs

--- API_PFR2/Presentation/API_REST/DTO/Requests/LoginRequest.cs
+++ API_PFR2/Presentation/API_REST/DTO/Requests/LoginRequest.cs
@@ -1,18 +1,23 @@
-﻿namespace API_PFR2.Presentation.API_REST.DTO.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_PFR2.Presentation.API_REST.DTO.Requests;
+
+/// <summary>
+/// Represents the data required to authenticate a user.
+/// </summary>
+public class LoginRequest
 {
     /// <summary>
-    /// Represents the data required to authenticate a user.
+    /// Gets or sets the email address of the user.
     /// </summary>
-    public class LoginRequest
-    {
-        /// <summary>
-        /// Gets or sets the email address of the user.
-        /// </summary>
-        public required string Email { get; set; }
+    [Required(ErrorMessage = "L'email est obligatoire.")]
+    [EmailAddress(ErrorMessage = "L'email n'est pas valide.")]
+    public required string Email { get; set; }
 
-        /// <summary>
-        /// Gets or sets the password provided by the user.
-        /// </summary>
-        public required string Password { get; set; }
-    }
+    /// <summary>
+    /// Gets or sets the password provided by the user.
+    /// </summary>
+    [Required(ErrorMessage = "Le mot de passe est obligatoire.")]
+    [MinLength(6, ErrorMessage = "Le mot de passe doit contenir au moins 6 caractères.")]
+    public required string Password { get; set; }
 }
--- API_PFR2/Presentation/API_REST/DTO/Requests/ReservationRequest.cs
+++ API_PFR2/Presentation/API_REST/DTO/Requests/ReservationRequest.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace API_PFR2.Presentation.API_REST.DTO.Requests;
 
 /// <summary>
@@ -9,20 +10,26 @@ public class CreateReservationRequest
     /// <summary>
     /// Gets or sets the identifier of the user making the reservation.
     /// </summary>
+    [Required(ErrorMessage = "L'identifiant de l'utilisateur est obligatoire.")]
+    [Range(1, int.MaxValue, ErrorMessage = "L'identifiant de l'utilisateur doit être supérieur à 0.")]
     public int UtilisateurId { get; set; }
 
     /// <summary>
     /// Gets or sets the identifier of the reserved game.
     /// </summary>
+    [Required(ErrorMessage = "L'identifiant du jeu est obligatoire.")]
+    [Range(1, int.MaxValue, ErrorMessage = "L'identifiant du jeu doit être supérieur à 0.")]
     public int JeuId { get; set; }
 
     /// <summary>
     /// Gets or sets the reservation start date.
     /// </summary>
+    [Required(ErrorMessage = "La date de début est obligatoire.")]
     public DateTime DateDebut { get; set; }
 
     /// <summary>
     /// Gets or sets the reservation end date.
     /// </summary>
+    [Required(ErrorMessage = "La date de fin est obligatoire.")]
     public DateTime DateFin { get; set; }
 }
